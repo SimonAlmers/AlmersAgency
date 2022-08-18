2569d498e6b29fc0ffae202e25f72140418913f0
Simon Almers <account@simonalmers.com>
2022-08-18 13:44:04 +0200

    Update component demo boiler plate

--- src/components/Blocks/ComponentDemoBlock/ComponentDemoBlock.tsx
+++ src/components/Blocks/ComponentDemoBlock/ComponentDemoBlock.tsx
@@ -8,31 +8,48 @@ const StyledBlock = styled("section", {
   padding: 48,
   minHeight: "70vh",
   display: "flex",
-  flexDirection: "column",
   justifyContent: "center",
   alignItems: "center",
+  columnGap: 48,
   h1: {
-    fontSize: "8rem",
+    fontSize: "2rem",
+  },
+  ".component": {
+    backgroundColor: "white",
+    borderRadius: 24,
+    width: 350,
+    height: 200,
+  },
+  ".info": {
+    "p, h1": {
+      margin: 0,
+    },
+    maxWidth: 500,
+    display: "flex",
+    flexDirection: "column",
+    rowGap: 32,
   },
 });
 
-const ComponentDemoBlock = ({ bgImage }: { bgImage?: string }) => {
+const ComponentDemoBlock = ({
+  order = "normal",
+}: {
+  order?: "normal" | "reversed";
+}) => {
   return (
     <StyledBlock>
-      <h1>Recent Articles</h1>
       <div
-        style={{
-          display: "flex",
-          alignItems: "start",
-          justifyContent: "center",
-          columnGap: 32,
-        }}
-      >
-        <ArticleCard article={articleList["54398"]} />
-        <ArticleCard
-          style={{ marginTop: 100 }}
-          article={articleList["87927"]}
-        />
+        style={{ order: order === "reversed" ? 1 : 0 }}
+        className="component"
+      />
+      <div className="info">
+        <h1>Component Name</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+          quisquam ipsa obcaecati optio quo architecto dolor velit eligendi
+          alias unde!
+        </p>
+        <button>Button</button>
       </div>
     </StyledBlock>
   );
--- src/pages/components/index.tsx
+++ src/pages/components/index.tsx
@@ -3,10 +3,10 @@ import ComponentDemoBlock from "components/Blocks/ComponentDemoBlock";
 const ComponentListViewPage = () => {
   return (
     <div>
-      <h1>Component List View</h1>
       <ComponentDemoBlock />
+      <ComponentDemoBlock order="reversed" />
       <ComponentDemoBlock />
-      <ComponentDemoBlock />
+      <ComponentDemoBlock order="reversed" />
       <ComponentDemoBlock />
     </div>
   );
